4464187a052f91d2c04706ea6df156be6f3bddee
Nikolay Tsybulskiy <104275092+KliffHenger@users.noreply.github.com>
2023-02-21 01:16:15 +0300

    config не поменял на прод

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 
 
-# TOKEN_API = "6173785703:AAGcYoU9lVs5XHmD566aJBO_HDLtuN6j8Yo" # токен Kufar_bot (@KufarForIlyaBot)
-TOKEN_API = "5801124476:AAGPSljD9fxksGJINgGmyAA9lwfW5kKn7xk" # токен all_test_bot (@my86352_bot)
+TOKEN_API = "6173785703:AAGcYoU9lVs5XHmD566aJBO_HDLtuN6j8Yo" # токен Kufar_bot (@KufarForIlyaBot)
+# TOKEN_API = "5801124476:AAGPSljD9fxksGJINgGmyAA9lwfW5kKn7xk" # токен all_test_bot (@my86352_bot)
 
 
 bot = Bot(TOKEN_API)
