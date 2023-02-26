cc46f4108d8402bed64935b230aa1afd8af4ab3a
Nikolay Tsybulskiy <104275092+KliffHenger@users.noreply.github.com>
2023-02-26 11:35:37 +0300

    Илья нашел баг

--- utils/parsing.py
+++ utils/parsing.py
@@ -10,15 +10,15 @@ def urlify(s):
     return x
 
 def get_item(region, keyword):
-    try:
-        URL_TEMPLATE = f'https://www.kufar.by/{region}?cmp=0&ot=1&query={keyword}&sort=lst.d'
-        r = requests.get(URL_TEMPLATE)
-        soup = bs(r.text, "html.parser")
-        find_href = soup.find('section')# .select('a', class_='styles_wrapper__IMYdY')
-        find_price = soup.find('p', class_='styles_price__tiO8k').select('span')
-        x = find_href.a['href']
-        y = find_price[0].text
-        return x, y
-    except:
-        pass
+    urla = f'https://www.kufar.by/{region}?cmp=0&ot=1&query={keyword}&sort=lst.d'
+    r = requests.get(urla)
+    soup = bs(r.text, "html.parser")
+    find_href = soup.find('section')# .select('a', class_='styles_wrapper__IMYdY')
+    # find_price = soup.find('p', class_='styles_price__tiO8k').select('span')
+    find_price = soup.find('section').select('span')
+    x = find_href.a['href']
+    y = find_price[0].text
+    return x, y
+
+# print(get_item('l/r~shchuchin', 'шуруповерт'))
 
